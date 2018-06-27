c2e934032bb290de247417f021c76225f3475d15
Mujahid <mkhan9047@gmail.com>
2018-06-27 16:29:04 +0600

    Initial commit

---
+++ app/src/main/java/service/spot/com/thespot/Activities/LogIn.java
@@ -0,0 +1,15 @@
+package service.spot.com.thespot.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import service.spot.com.thespot.R;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
